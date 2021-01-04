d7a9a240c825e5a479e46daced0b0e65b49d8341
brian hung <brianlhung@gmail.com>
2021-01-04 05:02:46 -0800

    use helper function with highlightTree to implement runmode

--- src/highlight.ts
+++ src/highlight.ts
@@ -1,7 +1,7 @@
 import {xmlLanguage} from "@codemirror/lang-xml"
 import {runmode, appendToDOM} from "./runmode"
 
-/* if you want to use a legacy modes, do this:
+/* if you want to use a legacy mode, do this instead:
 import {xml} from "@codemirror/legacy-modes/mode/xml"
 import {StreamLanguage} from "@codemirror/stream-parser"
 const xmlLanguage = StreamLanguage.define(xml);
--- src/runmode.ts
+++ src/runmode.ts
@@ -3,7 +3,13 @@ import {highlightTree, defaultHighlightStyle} from "@codemirror/highlight"
 
 export function runmode(textContent: string, language: Language, callback: (text: string, style: string, from: number, to: number) => void, options?: Record<string, any>) {
   const tree = language.parseString(textContent);
-  highlightTree(tree, defaultHighlightStyle.match, (from, to, classes) => callback(textContent.slice(from, to), classes, from, to));
+  let pos = 0;
+  highlightTree(tree, defaultHighlightStyle.match, (from, to, classes) => {
+    from > pos && callback(textContent.slice(pos, from), null, pos, from);
+    callback(textContent.slice(from, to), classes, from, to);
+    pos = to;
+  });
+  pos != tree.length && callback(textContent.slice(pos, tree.length), null, pos, tree.length);
 }
 
 export function appendToDOM(parent: HTMLElement, options?: {tabSize?: number}) {
